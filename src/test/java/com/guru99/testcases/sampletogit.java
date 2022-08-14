d200f3a0fcef44d1efdbcbf592c5b1ab839e3d4c
hp <hp@DESKTOP-J585NT9>
2022-08-14 16:21:51 +0530

    TC 42.5 and 45.4

---
+++ src/test/java/com/guru99/testcases/sampletogit.java
@@ -0,0 +1,14 @@
+package com.guru99.testcases;
+
+import org.testng.annotations.Test;
+
+public class sampletogit {
+
+	@Test
+	public void deshkart() {
+		
+		System.out.println("git is successfull");
+		
+	}
+	
+}
